d43465fe566cfc0864b70e96e907911ff29a97f3
Xinzhan Bai <xb4zp@virginia.edu>
2023-10-18 16:21:25 -0400

    sync ifarm repo

--- cook_data.sh
+++ cook_data.sh
@@ -6,7 +6,10 @@
 
 #python3 bin/analysis_pipeline.py --raw-dir /cache/halla/solid/subsystem/ec/ecal_beamtest_hallc/raw --proc-dir ./processed_data MAPMT 3588
 
-python3 bin/analysis_pipeline.py --raw-dir /volatile/halla/solid/xbai --proc-dir /volatile/halla/solid/xbai/replayed_root_files MAPMT 4208
+#python3 bin/analysis_pipeline.py --raw-dir /volatile/halla/solid/xbai --proc-dir /volatile/halla/solid/xbai/replayed_root_files MAPMT 4208
 
 #python3 bin/analysis_pipeline.py --raw-dir /volatile/halla/solid/xbai --proc-dir /volatile/halla/solid/xbai/replayed_root_files MAPMT 4350
+
+python3 bin/analysis_pipeline.py --raw-dir /volatile/halla/solid/xbai --proc-dir /volatile/halla/solid/xbai/check1 MAPMT 4680
+
 
